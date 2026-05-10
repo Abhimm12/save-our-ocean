484b91e3d03568cc54a7f9b3d740242b0b9466c6
Abhimm12 <abhimanyunugroadi@gmail.com>
2026-05-10 19:24:41 +0700

    Delete SAVE OCEAN directory

--- SAVE OCEAN/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const donateBtn = document.getElementById("donateBtn");
-
-  if (donateBtn) {
-    donateBtn.addEventListener("click", () => {
-      alert("Terima kasih atas dukungan Anda untuk menyelamatkan hewan laut!");
-    });
-  }
-});
